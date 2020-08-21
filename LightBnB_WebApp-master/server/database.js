f5147fb4f18a4eed41bc859d3e2713ac69ebad46
Balkaran Jaswal <balkaranjaswal@gmail.com>
2020-08-21 01:58:24 +0000

    added webapp, redid getAllProperties function

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -1,6 +1,15 @@
 const properties = require('./json/properties.json');
 const users = require('./json/users.json');
-
+const pg = require('pg');
+const Pool = pg.Pool;
+const options = {
+  user: 'vagrant',
+  password: '123',
+  database: 'lightbnb',
+  host: 'localhost'
+}
+const pool = new Pool(options);
+pool.connect();
 /// Users
 
 /**
@@ -67,13 +76,15 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
-  const limitedProperties = {};
-  for (let i = 1; i <= limit; i++) {
-    limitedProperties[i] = properties[i];
-  }
-  return Promise.resolve(limitedProperties);
+  return pool.query(`
+  SELECT * FROM properties
+  LIMIT $1
+  `, [limit])
+  .then(res => res.rows);
 }
+
 exports.getAllProperties = getAllProperties;
+
 
 
 /**
